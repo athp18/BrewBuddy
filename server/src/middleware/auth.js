2fbd3d51013815daab286ca9f531042196be5104
athp18 <129908089+athp18@users.noreply.github.com>
2026-05-12 17:57:12 -0700

    Update auth.js

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -9,7 +9,7 @@ const protect = async (req, res, next) => {
   }
 
   if (!token) {
-    return res.status(401).json({ message: 'Not authorised — no token' });
+    return res.status(401).json({ message: 'Not authorized — no token' });
   }
 
   try {
